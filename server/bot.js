a0f7061215573f015acc619703d2c55c4f2eb450
abrikos <abrikoz@gmail.com>
2019-11-11 16:40:32 +0900

    proxy fix

--- server/bot.js
+++ server/bot.js
@@ -8,16 +8,16 @@ const options = {
     polling: true,
     request: {
         agentClass: Agent,
-        agentOptions: {
+        /*agentOptions: {
             // If authorization is needed:
             // socksUsername: process.env.PROXY_SOCKS5_USERNAME,
             // socksPassword: process.env.PROXY_SOCKS5_PASSWORD
-        }
+        }*/
     }
 };
 if(process.env.PROXY_SOCKS5_HOST && parseInt(process.env.PROXY_SOCKS5_PORT)){
-    options.request.agentOptions.socksHost = process.env.PROXY_SOCKS5_HOST
-    options.request.agentOptions.socksPort = parseInt(process.env.PROXY_SOCKS5_PORT)
+    console.log(process.env)
+    options.request.agentOptions = {socksHost : process.env.PROXY_SOCKS5_HOST, socksPort : parseInt(process.env.PROXY_SOCKS5_PORT)}
 }
 const bot = new TelegramBot(process.env.BOT_TOKEN, options);
 BotProcess.run(bot);
