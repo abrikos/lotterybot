538718bcf6949331454f12e3d4405e3b4ce6eb45
abrikos <abrikoz@gmail.com>
2019-11-11 16:50:15 +0900

    pm2 fix

--- pm2.production.config.js
+++ pm2.production.config.js
@@ -1,5 +1,5 @@
-const pkginfo = require ('./package.json');
-const execSync = require ('child_process').execSync;
+const pkginfo = require('./package.json');
+const execSync = require('child_process').execSync;
 
 const log_date_format = 'DD.MM.YYYY HH:mm:ss';
 const combine_logs = true;
@@ -7,29 +7,32 @@ const combine_logs = true;
 let branch = '';
 
 try {
-	let gitInfo = execSync ('git log -1 --date="format:%Y.%m.%d:%H%M" --format="%cd %D"') + '';
-	branch = gitInfo.split (',')[1].split('/').pop ();
-} catch (error) {};
+    let gitInfo = execSync('git log -1 --date="format:%Y.%m.%d:%H%M" --format="%cd %D"') + '';
+    branch = gitInfo.split(',')[1].split('/').pop();
+} catch (error) {
+}
+;
 
 /**
  * Конфигурация для деплоя
  */
 module.exports = {
-	apps : [{
-		name: `${pkginfo.name}-bot`,
-		script: `${__dirname}/server/bot.js`,
-		node_args: '--preserve-symlinks -r esm',
-		env: {
-			NODE_ENV: 'production',
-			SOURCE_MAP: 'source-map',
-			//DEBUG: '*',
-		},
-		output: 'logs/bot.log',
-		error: 'logs/bot-error.log',
-		//log_date_format,
-		//combine_logs,
-		//error_file: `${__dirname}/logs/server.err.log`,
-		//out_file:   `${__dirname}/logs/server.out.log`,
-		//pid_file:   `${__dirname}/logs/server.pid`,
-	}]
+    apps: [{
+        name: `${pkginfo.name}-bot`,
+        script: `${__dirname}/server/bot.js`,
+        node_args: '--preserve-symlinks -r esm',
+        env: {
+            NODE_ENV: 'production',
+            SOURCE_MAP: 'source-map',
+            NODE_PATH: '.',
+            //DEBUG: '*',
+        },
+        output: 'logs/bot.log',
+        error: 'logs/bot-error.log',
+        //log_date_format,
+        //combine_logs,
+        //error_file: `${__dirname}/logs/server.err.log`,
+        //out_file:   `${__dirname}/logs/server.out.log`,
+        //pid_file:   `${__dirname}/logs/server.pid`,
+    }]
 };
