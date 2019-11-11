9670740300338b3db7e05da14ddb6e3538d120cf
abrikos <abrikoz@gmail.com>
2019-11-11 20:23:09 +0900

    init user wallets

--- server/initialize.js
+++ server/initialize.js
@@ -1,14 +1,14 @@
 import {Configurator} from "server/lib/Configurator";
 const mongoose = require('server/lib/mongoose');
 async function init() {
-    await mongoose.Wallet.deleteMany({});
-    await mongoose.Lottery.deleteMany({});
-    await mongoose.Transaction.deleteMany({});
-    await mongoose.Payment.deleteMany({});
+    //await mongoose.Wallet.deleteMany({});
+    //await mongoose.Lottery.deleteMany({});
+    //await mongoose.Transaction.deleteMany({});
+    //await mongoose.Payment.deleteMany({});
     for (const network of Configurator.getKeys()) {
         const App = new Configurator(network);
         const l = await mongoose.Lottery.findOne({finisTime: 0, network});
-        if (!l) await App.lotteryCreate();
+        //if (!l) await App.lotteryCreate();
 
         const users = await mongoose.User.find().populate('wallets')
 
