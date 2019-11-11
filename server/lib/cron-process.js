fad4c2b8bfcb2efdab237610f907c26a9d8d66dc
abrikos <abrikoz@gmail.com>
2019-11-11 20:02:46 +0900

    log transactions

--- server/lib/cron-process.js
+++ server/lib/cron-process.js
@@ -36,7 +36,7 @@ export default {
                 .populate(mongoose.Wallet.population);
             for (const wallet of wallets) {
                 const App = new Configurator(wallet.network);
-                logger.info('Wallet', App.crypto.getAddressLink(wallet.address))
+                logger.info('Wallet', wallet.network)
                 const transactions = await App.crypto.loadTransactions(wallet.address);
                 for (const transaction of transactions) {
                     logger.info('TX check', App.crypto.getTransactionLink(transaction.hash))
