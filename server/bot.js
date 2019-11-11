356292f4562e34663d198641654fd7e4603424cc
abrikos <abrikoz@gmail.com>
2019-11-11 16:36:52 +0900

    proxy fix

--- server/bot.js
+++ server/bot.js
@@ -4,18 +4,21 @@ const TelegramBot = require('node-telegram-bot-api');
 const Agent = require('socks5-https-client/lib/Agent');
 require('dotenv').config()
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {
+const options = {
     polling: true,
     request: {
         agentClass: Agent,
         agentOptions: {
-            socksHost: process.env.PROXY_SOCKS5_HOST,
-            socksPort: parseInt(process.env.PROXY_SOCKS5_PORT),
             // If authorization is needed:
             // socksUsername: process.env.PROXY_SOCKS5_USERNAME,
             // socksPassword: process.env.PROXY_SOCKS5_PASSWORD
         }
     }
-});
+};
+if(process.env.PROXY_SOCKS5_HOST && parseInt(process.env.PROXY_SOCKS5_PORT)){
+    options.request.agentOptions.socksHost = process.env.PROXY_SOCKS5_HOST
+    options.request.agentOptions.socksPort = parseInt(process.env.PROXY_SOCKS5_PORT)
+}
+const bot = new TelegramBot(process.env.BOT_TOKEN, options);
 BotProcess.run(bot);
 CronProcess.run(bot);
