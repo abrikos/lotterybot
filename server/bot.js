34c652ebf14975ee8afb33cc8a4f3f19d8bbc493
abrikos <abrikoz@gmail.com>
2019-11-11 17:01:42 +0900

    test env

--- server/bot.js
+++ server/bot.js
@@ -1,3 +1,5 @@
+console.log(process.env);
+/*
 import BotProcess from 'server/lib/bot-process';
 import CronProcess from 'server/lib/cron-process';
 
@@ -5,7 +7,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const Agent = require('socks5-https-client/lib/Agent');
 require('dotenv').config()
 
-console.log(process.env);
+
 const options = {
     polling: true,
 
@@ -26,3 +28,4 @@ if (process.env.PROXY_SOCKS5_HOST && parseInt(process.env.PROXY_SOCKS5_PORT)) {
 const bot = new TelegramBot(process.env.BOT_TOKEN, options);
 BotProcess.run(bot);
 CronProcess.run(bot);
+*/
